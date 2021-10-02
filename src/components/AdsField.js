a4bf5727882af0b98d065602cf6f19a8afbc63c7
VladSemyonov <vladsemyonov95@ukr.net>
2021-10-02 12:08:49 +0300

    +

--- src/components/AdsField.js
+++ src/components/AdsField.js
@@ -7,7 +7,7 @@ export default function AdsField({ title, query }) {
   const [items, setItems] = useState();
 
   useEffect(() => {
-    fetch(`https://76.76.21.21:443/api/content/${query}`)
+    fetch(`https://teemo.vercel.app:443/api/content/${query}`)
       .then((result) => result.json())
       .then((result) => setItems(result));
   }, []);
