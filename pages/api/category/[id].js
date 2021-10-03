6d2f4eca3992e301c320993931c275934a7f7bba
VladSemyonov <vladsemyonov95@ukr.net>
2021-10-03 14:14:12 +0300

    -

--- pages/api/category/[id].js
+++ pages/api/category/[id].js
@@ -2,11 +2,14 @@ import { data } from "../../../src/data";
 
 export default function handler(req, res) {
   const { pid } = req.query;
-  let response = data.productsArr.Сантехника.filter(
-    (item) =>
-      (item.categoryId._attributes.id === pid ||
-        item.categoryId._attributes.parentId === pid) &&
-      item
-  );
+  let response = data.productsArr.Сантехника.filter((item) => {
+    if (
+      item.categoryId._attributes.id === pid ||
+      item.categoryId._attributes.parentId === pid
+    ) {
+      console.log("true");
+      return item;
+    }
+  });
   res.status(200).json(response);
 }
