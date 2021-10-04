02a134edbff96e72350093ea6b09d152a32d6dd3
VladSemyonov <vladsemyonov95@ukr.net>
2021-10-05 00:06:27 +0300

    fix product

--- pages/api/product/[id].js
+++ pages/api/product/[id].js
@@ -2,6 +2,8 @@ import { data } from "../../../src/data";
 
 export default function handler(req, res) {
   const { id } = req.query;
-  let responce = data.productsArr.filter((i) => i._attributes.id === id);
+  let responce = data.productsArr.Сантехника.filter(
+    (i) => i._attributes.id === id
+  );
   res.status(200).json(responce);
 }
